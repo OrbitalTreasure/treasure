360ce8673244ea01d8fe55375f762c2f22360156
Joel Chan <joelchanzhiyang@gmail.com>
2021-05-18 12:54:18 +0800

    added routing

--- loginpoc/src/App.js
+++ loginpoc/src/App.js
@@ -1,53 +1,20 @@
-import './App.css';
-import Login from './Login'
+import "./App.css";
+import Homepage from "./Homepage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CallbackLogic from "./CallbackLogic"
 
 function App() {
-  const postTextInput = document.querySelector("#post");
-  
-  const getRedditPostData = e => {
-      e.preventDefault();
-      const textInput = postTextInput.value;
-  
-      if (!isURL(textInput)) {
-          fetchRedditPostInfo(textInput);
-      } else {
-          const urlObject = new URL(textInput);
-          fetchRedditPostInfo(urlObject.pathname.split("/")[4]);
-      }
-  }
-  
-  const fetchRedditPostInfo = postID => {
-      const url =`https://api.reddit.com/api/info/?id=t3_${postID}`;
-      fetch(url, {mode:'cors'})
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(e => console.log(e))
-  }
-  
-  function isURL(str) {
-      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-      return !!pattern.test(str);
-  }
-  
-  const insertExample = e => postTextInput.value = "https://www.reddit.com/r/news/comments/ndyvkx/now_is_not_the_time_nurses_union_condemns_cdc_for/?utm_source=share&utm_medium=web2x&context=3"
-
   return (
-    <div className="card">
-        <h1>Reddit Login Test Page</h1>
-        <form onSubmit={getRedditPostData}>
-            <input type="text" id="post" placeholder="Insert Reddit Post ID or URL"/>
-            <input type="submit" value="Get Post Details" id="submitButton"/>
-        </form>
-        <input type="button" value="Insert example url" id="exampleButton" onSubmit={insertExample}/>
-        <br/>
-        <br/>
-        <Login/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/auth-callback">
+          <CallbackLogic/>
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- loginpoc/src/Login.js
+++ loginpoc/src/Login.js
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 const snoowrap = require('snoowrap');
 
 function Login() {
-    // eslint-disable-next-line
-    const [redditInstance, setRedditInstance] = useState(null);
-
     function redirectToLogin(e) {
         const url = snoowrap.getAuthUrl({
             clientId: "DzfZOF3d3768Yw",
             scope: ["identity"],
-            redirectUri: "http://localhost:3000/callback/",
+            redirectUri: "http://localhost:3000/",
             permanant: true,
             state: "123"
         })
         window.open(url);
     }
-
 
     return (
         <input type="button" value="Login in Using Reddit" onClick={redirectToLogin}></input>
