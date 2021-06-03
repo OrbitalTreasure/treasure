2123230fc80155666775bf47c08818aec9c0437d
Joel Chan <joelchanzhiyang@gmail.com>
2021-06-03 13:56:20 +0800

    commited contracts

---
+++ blockchain/contracts/TreasureTokenFactory.sol
@@ -0,0 +1,57 @@
+//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract TreasureTokenFactory is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("TreasureOrbital", "TOR") {}
+
+    struct RedditOffer {
+      bytes32 buyerId;
+      bytes32 sellerId;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
+
+
+   // //Emitted when update function is called
+   // //Smart contract events are a way for your contract to communicate that something happened on the blockchain to your app front-end, which can be 'listening' for certain events and take action when they happen.
+   // event UpdatedMessages(string oldStr, string newStr);
+
+   // // Declares a state variable `message` of type `string`.
+   // // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
+   // string public message;
+
+   // // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
+   // // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+   // constructor(string memory initMessage) {
+   
+   //    // Accepts a string argument `initMessage` and sets the value into the contract's `message` storage variable).
+   //    message = initMessage;
+   // }
+
+   // // A public function that accepts a string argument and updates the `message` storage variable.
+   // function change(string memory newMessage) public {
+   //    string memory oldMsg = message;
+   //    message = newMessage;
+   //    emit UpdatedMessages(oldMsg, newMessage);
+   // }
