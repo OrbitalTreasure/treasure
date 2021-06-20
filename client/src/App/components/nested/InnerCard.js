0f7972a01add48831cf8a6bde6118e944fa1d8fe
Richard Li Defa <richardlidefa@gmail.com>
2021-06-20 12:44:11 +0800

    changes

--- client/src/App/components/nested/InnerCard.js
+++ client/src/App/components/nested/InnerCard.js
@@ -10,27 +10,27 @@ const InnerCard = (props) => {
   };
 
   return (
-    <div className="container" onClick={redirectToPost(props.id)}>
-      <h4 className="heading">
+    <div className="container">
+      <h4 className="heading" onClick={redirectToPost(props.id)}>
         {props.title}
       </h4>
       <div className="subHeading">
         <p className="subreddit">{"/r/" + props.subreddit}</p>
-        <NavLink activeClassName="author" to={`/user/${props.author}`}>
+        <NavLink activeClassName="author" to={`/user/${props.authorId}`}>
           Post by u/{props.author}
         </NavLink>
       </div>
-      {(props?.selftext || props?.url) && (
+      {(props?.selftext || props?.imageUrl) && (
         <div className="content">
           {props?.selftext && <div className="bodyText">{props?.selftext}</div>}
-          {!props?.selftext && props?.url && (
-            <img className="bodyImg" src={props.url} />
+          {!props?.selftext && props?.imageUrl && (
+            <img className="bodyImg" src={props.imageUrl} />
           )}
         </div>
       )}
       <div className="upvote">
         <img className="upvoteImg" src={upvoteImg} />
-        <p className="upvoteCount">{props.score}</p>
+        <p className="upvoteCount">{props.upvotes}</p>
       </div>
     </div>
   );
