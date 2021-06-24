e733011ba460cc87483c0886ab90d0fe2dd0dd21
Joel Chan <joelchanzhiyang@gmail.com>
2021-06-24 13:13:54 +0800

    changed the build file location

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 4000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.use("/api/v1", require("../routes/backend/api.js"));
 
@@ -15,6 +14,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
+const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log("ExpressJS Server Running at http://localhost:" + port);
 });
